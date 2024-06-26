ec246128b4ef5fa6782325e8bb55b6e81e25f720
yxfan <yxfan@streamax.com>
2024-06-26 18:39:12 +0800

    feat：变量控制元素的隐藏时的处理

--- Day2/vite-runner/App.jsx
+++ Day2/vite-runner/App.jsx
@@ -1,7 +1,7 @@
 /*
  * @Author: yxfan
  * @Date: 2024-06-01 15:17:35
- * @LastEditTime: 2024-06-26 17:30:38
+ * @LastEditTime: 2024-06-26 18:14:23
  * @FilePath: /Day2/vite-runner/App.jsx
  * @Description: 
  */
@@ -9,7 +9,7 @@ import React from "./core/React.js";
 // 这里vite会把jsx默认转化成 React.createElement方法调用
 
 let count = 0;
-let showBar = true;
+let showBar = false;
 function Count({ num }) {
     function hnadleClick() {
         count++;
@@ -36,16 +36,16 @@ function ShowBar() {
         </div>
     )
     return (
-        <div id="yxfan">
-        
-            <div>{showBar ? foo : <div>bar</div>}</div>
+        <div id="yxfan2">
+            {/* <div>{showBar ? foo : <div>bar</div>}</div> */}
+            {showBar && foo}
             <button onClick={handleShow}>切换</button>
         </div>
     )
 }
 function App() {
     return (
-        <div id="yxfan"> 
+        <div id="yxfan1"> 
             {/* 10 */}
             {/* app */}
             {/* <h2>T</h2> */}
